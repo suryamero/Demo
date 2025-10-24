e0a50247ab110e29d787ee770d0a2386378b45a9
surya <suryamero@gmail.com>
2025-10-23 19:53:09 -0700

    Initial commit to new API repo

--- src/main/java/org/example/EncapsulDemo.java
+++ src/main/java/org/example/EncapsulDemo.java
@@ -10,6 +10,7 @@ class Employee{
     public int getAge(){
         return age;
     }
+
     public void setName(String n) {
         this.Name=n;
     }
@@ -23,6 +24,7 @@ public class EncapsulDemo {
     public static void main(String[] args) {
         Employee Emp=new Employee();
         Emp.setName("John");
+
         System.out.println("Employee Name is: "+Emp.getName());
          Emp.setAge(12);
          System.out.println("Employee Age is: "+Emp.getAge());
